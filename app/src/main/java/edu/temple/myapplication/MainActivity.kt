e0a4d986a8d38d78cc15c31c991daae8eed9b464
johalloran01 <joshuamohalloran@gmail.com>
2023-11-09 11:54:21 -0500

    Implemented TimberBinder in main. Moved serviceConnection outside of onCreate function to fix initial crash issues.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,25 +1,51 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerbinder : TimerService.TimerBinder
+    var isConnected = false
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerbinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
 
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) timerbinder.start(100)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) timerbinder.pause()
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
 
+        findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if (isConnected) timerbinder.stop()
         }
     }
 }
