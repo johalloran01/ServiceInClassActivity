323f19e2f7ec212bcdd1ae3752b7c81e5af7ba9e
johalloran01 <joshuamohalloran@gmail.com>
2023-11-28 11:53:07 -0500

    Never committed to a separate branch before.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 
@@ -34,7 +36,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_play_button -> if (isConnected) timerbinder.start(100)
+            R.id.action_pause_button -> if (isConnected) timerbinder.pause()
+            R.id.action_stop_button ->  if (isConnected) timerbinder.stop()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         timerTextView = findViewById(R.id.timerTextView)
